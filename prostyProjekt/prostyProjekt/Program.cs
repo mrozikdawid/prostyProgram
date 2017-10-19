5927ba104fc6c924a84208357606695cbffb958f
mrozikdawid <mrozik.dawid@gmail.com>
2017-10-19 14:25:06 +0200

    Add files via upload

---
+++ prostyProjekt/prostyProjekt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prostyProjekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c,wynik;
+            Console.WriteLine("Podaj a: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj b: ");
+            b = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj c: ");
+            c = Int32.Parse(Console.ReadLine());
+            wynik = a * b * c;
+
+            Console.WriteLine(wynik);
+            Console.ReadKey();
+        }
+        
+    }
+}
